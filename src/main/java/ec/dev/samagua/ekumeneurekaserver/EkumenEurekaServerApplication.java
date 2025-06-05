92cc2650c1ba78626fc78ba816540fd963a25eba
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-06-04 21:36:39 -0500

    config added

--- src/main/java/ec/dev/samagua/ekumeneurekaserver/EkumenEurekaServerApplication.java
+++ src/main/java/ec/dev/samagua/ekumeneurekaserver/EkumenEurekaServerApplication.java
@@ -2,8 +2,10 @@ package ec.dev.samagua.ekumeneurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EkumenEurekaServerApplication {
 
     public static void main(String[] args) {
